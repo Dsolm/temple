bf145fd00ce0a46c6e5bf4fa00316c249424fe0d
David <pepotetm0@gmail.com>
2023-06-22 01:15:12 +0200

    Improved static inlining.

--- src/temple-tests.lisp
+++ src/temple-tests.lisp
@@ -14,4 +14,4 @@
   (is (equal (a) "<a></a>"))
   (is (equal (a (+ 1 (+ 1 1))) "<a>3</a>"))
   (is (equal (a :href (+ 1 (+ 1 1)) :class (+ 1 (+ 1 1)) (+ 1 (+ 1 1)) (+ 1 3)) "<a class='3' href='3'>34</a>"))
-  (is (equal (macroexpand '(a :class nunu :href "hi" "hi" *nunu*)) '(FORMAT NIL "<a href='hi' class='~a'>hi~a</a>" NUNU *NUNU*))))
+  (is (equal (macroexpand '(a :class nunu :href "hi" "hi" *nunu*)) '(format nil "<a href='hi' class='~a'>hi~[~{~a~}~]</a>" nunu (list *nunu*)))))
--- src/temple.lisp
+++ src/temple.lisp
@@ -3,12 +3,16 @@
 
 (in-package :temple)
 
+(defun callablep (list)
+  (and (listp list) (symbolp (car list))
+       (not (equal (car list) 'quote))
+       (not (equal (car list) 'list))))
+
 ; A symbol is a special variable if it can be evaluated.
-(defun specialp (sym)
-  "Checks if `sym' is a special variable (global)"
-  (when (not (constantp sym))
-   (handler-case (and (eval sym) t)
-    (error () nil))))
+(defun specialp (var)
+  "Checks if `var' is a special variable (global) or contains one."
+  (cond ((and (symbolp var) (not (constantp var))) (handler-case (and (eval var) t) (error () nil)))
+        ((listp var) (when (find-if #'specialp var) t))))
 
 (defun eval-at-runtimep (arg)
   (if (specialp arg)
@@ -21,11 +25,12 @@
     (loop for item in contents do
           (if (eval-at-runtimep item)
             (progn
-              (setf str (concatenate 'string str "~a"))
+              (setf str (concatenate 'string str "~@[~{~a~}~]"))
               (uiop:appendf args (list item)))
             (setf str (concatenate 'string str (format nil "~a" item)))))
     (values str args)))
 
+; TODO: interpolate-static-args does not work with runtime lists.
 (defun interpolate-static-args (args)
   (when args
     (multiple-value-bind (str parameters) (interpolate-static-args (cddr args))
@@ -40,33 +45,37 @@
 Interpolates as many expressions at compile time as possible. Special (global) variables won't be interpolated at compile time.
 
  E.g:  (tag \"a\" (:href \"hi\" \"hello\")) => \"<a href='hi'>hello</a>\" "
+
+  ; evaluate any functions embedded in the html tag.
+  (setf params-and-contents (mapcar (lambda (x)
+                                      (if (equal :comptime (and (listp x) (car x)))
+                                        (eval (second x))
+                                        (if (specialp x)
+                                            x
+                                            (handler-case (eval x) (error () x))))) params-and-contents))
+
   (let ((prev) (args) (contents))
     (loop for val in params-and-contents do
-      (cond
-        ((keywordp prev)
-         (when (find-if (lambda (x) (equal prev x)) args)
-           (warn "Repeated arguments."))
-         (uiop:appendf args (list (sb-unicode:lowercase (string prev)) val)))
-        ((not (keywordp val)) (uiop:appendf contents (list val))))
+      (when val
+        (cond
+          ((keywordp prev)
+           (when (find-if (lambda (x) (equal prev x)) args)
+             (warn "Repeated arguments."))
+           (uiop:appendf args (list (sb-unicode:lowercase (string prev)) val)))
+          ((and (not (keywordp val)) (listp val) (not (callablep val))) (uiop:appendf contents val))
+          ((not (keywordp val)) (uiop:appendf contents (list val)))))
       (setf prev val))
-
-    ; evaluate any functions embedded in the html tag.
-    (setf args (mapcar (lambda (x) (if (specialp x)
-                                  x
-                                  (handler-case (eval x) (error () x)))) args))
-    (setf contents (mapcar (lambda (x) (if (specialp x)
-                                      x
-                                      (handler-case (eval x) (error () x)))) contents))
 
     ; Pregenerate as much text as possible at compile time.
     (multiple-value-bind (arg-template arg-params) (interpolate-static-args args)
       (multiple-value-bind (content-template content-params) (interpolate-static-contents contents)
-          (let ((template (format nil "<~a~@[~a~]>~@[~a~]</~a>" key arg-template content-template key)))
-            (cond
-              ((and content-params arg-params) `(format nil ,template ,@arg-params ,@content-params))
-              (arg-params `(format nil ,template ,@arg-params))
-              (content-params `(format nil ,template ,@content-params))
-              (t template)))))))
+        (let ((template (format nil "<~a~@[~a~]>~@[~a~]</~a>" key arg-template content-template key)))
+          (cond
+            ((and content-params arg-params) `(format nil ,template ,@arg-params ,@(loop for param in content-params
+                                                                                         collect `(list ,param))))
+            (arg-params `(format nil ,template ,@arg-params))
+            (content-params `(format nil ,template ,@content-params))
+            (t template)))))))
 
 (defmacro deftags (&rest tags)
   (when tags
